03120fd4b9c40ac6015d32fa49654e9b3167bebf
Artem Tarasov <tarasovartems@gmail.com>
2023-10-08 22:24:28 +0300

    Home commit

---
+++ hello.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var lang := ReadString('Введите язык, на котором говорите');
+  if lang = 'English' then print('Hello')
+  else if lang = 'Deutsch' then print('Hallo')
+  else if lang = 'Français' then print('Bonjour');
+end.
